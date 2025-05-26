009b0879056d52a7850177cd4311c24c1cb28f56
itc <itcnotificationsdailytest@gmail.com>
2025-05-26 19:28:59 +0530

    commited of Uploadconstants

--- backend/src/main/java/com/amazon/backend/constant/UploadFileConstants.java
+++ backend/src/main/java/com/amazon/backend/constant/UploadFileConstants.java
@@ -8,5 +8,9 @@ public class UploadFileConstants {
 	public static final String FILE_UPLOADED_SUCCESSFULLY = "File has been uploaded successfully";
 	
 	public static final String STROING_METADATA_OF_UPLOADED_FILE = "Storing metadata of uploaded file into db";
+	
+	public static final String STROING_METADATA_OF_UPLOADED_FILE2 = "Storing metadata of uploaded file into db";
+	
+	public static final String STROING_METADATA_OF_UPLOADED_FILE3 = "Storing metadata of uploaded file into db";
 
 }
